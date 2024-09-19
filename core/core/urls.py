f3fd1fd10f203aa140be012691cbad317935edeb
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-09-19 15:13:32 +0330

    [ADD] accounts urls & swagger url

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,13 +15,40 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog api",
+        default_version="v1",
+        description="a test document for the blog api",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("api-docs/", include_docs_urls(title="api_sample")),
+    path("swagger/o.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
 
